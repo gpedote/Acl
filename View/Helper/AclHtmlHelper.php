a4d39f82fa6f178f043e001376eb402dc3519b91
Gabriel Pedote <gabriel.pedote@fatec.sp.gov.br>
2014-02-06 17:24:16 -0200

    Fixed style and improved performance of HtmlHelper/link

--- View/Helper/AclHtmlHelper.php
+++ View/Helper/AclHtmlHelper.php
@@ -4,15 +4,15 @@ App::uses('HtmlHelper', 'View/Helper');
 
 class AclHtmlHelper extends HtmlHelper {
 
-	var $helpers = array('Session');
+	public $helpers = array('Session');
 
-	function link($title, $url = null, $options = array(), $confirmMessage = false) {
+	public function link($title, $url = null, $options = array(), $confirmMessage = false) {
 		$permissions = $this->Session->read('Alaxos.Acl.permissions');
 		if (!isset($permissions)) {
-			$permissions = array();
+			return null;
 		}
 
-		$aco_path = AclRouter :: aco_path($url);
+		$aco_path = AclRouter::aco_path($url);
 
 		if (isset($permissions[$aco_path]) && $permissions[$aco_path] == 1) {
 			return parent::link($title, $url, $options, $confirmMessage);
