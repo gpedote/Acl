831a3c70e72f7649a29f6f44503d8d76aede1112
Gabriel Pedote <gabriel.pedote@fatec.sp.gov.br>
2014-03-31 16:54:35 -0300

    Added Tests

---
+++ Test/Case/Lib/AclRouterTest.php
@@ -0,0 +1,18 @@
+<?php
+App::uses('AclRouter', 'Acl.Lib');
+
+/**
+ * AclRouter Test Case
+ *
+ */
+class AclRouterTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * testAcoPath method
+ *
+ * @return void
+ */
+    public static function testAcoPath() {
+        
+    }
+}
